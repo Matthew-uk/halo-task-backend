c6aea9048663ad5a6a20680c1c9c067d5d26cc5f
Matthew-uk <esaduviedede@gmail.com>
2024-01-17 10:43:52 +0100

    Updating Cors Options

--- index.js
+++ index.js
@@ -5,7 +5,22 @@ const dotenv = require("dotenv");
 const { userRouter } = require("./routers/router");
 const mongoose = require("mongoose");
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000/",
+  "https://thehalotask.vercel.app/",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 dotenv.config();
 
---
+++ models/taskModel.js
@@ -0,0 +1,25 @@
+const { default: mongoose } = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    taskName: {
+      type: String,
+      required: true,
+    },
+    taskDescription: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Tasks", taskSchema);
