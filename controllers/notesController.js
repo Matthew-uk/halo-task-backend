c27bff37e872136f57c12fbc411d814063e4dac5
Matthew-uk <esaduviedede@gmail.com>
2024-01-21 05:50:24 +0100

    Updating Note Get request to take in User Id instead

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -14,7 +14,7 @@ const handleCreateNote = expressAsyncHandler(async (req, res) => {
 
 const handleGetNote = expressAsyncHandler(async (req, res) => {
   const { id } = req.query;
-  const note = await notesModel.findById(id);
+  const note = await notesModel.find({ userId: id });
   res.status(200).json(note);
 });
 
